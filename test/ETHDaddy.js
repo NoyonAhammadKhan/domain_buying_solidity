20f2978bd66dd6472aa60f8a604a6974f4e5c56a
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2023-01-21 06:13:36 -0500

    initial commit

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,37 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("ETHDaddy", () => {
+  let ethDaddy;
+  let deployer, owner1;
+  const NAME = 'ETH Daddy';
+  const SYMBOL = 'ETHD';
 
+
+  beforeEach(async()=>{
+     [deployer,owner1]= await ethers.getSigners();
+    console.log(deployer,owner1);
+    const ETHDaddy = await ethers.getContractFactory('ETHDaddy');
+    ethDaddy = await ETHDaddy.deploy('ETH Daddy','ETHD');
+  })
+
+ 
+
+  describe('Deployment',()=>{
+    it('has a name',async()=>{
+
+      let result = await ethDaddy.name();
+      expect(result).to.equal(NAME);
+  
+    })
+  
+    it('has a symbol',async()=>{
+      let result = await ethDaddy.symbol();
+      expect(result).to.equal(SYMBOL);
+    })
+  })
 })
